7caa99d5a336721970da83423c237d56e0be8e6a
npm27 <nick.maxwell61@yahoo.com>
2020-03-25 08:30:38 -0500

    Setting up to host EX 2 online

---
+++ Ex 2/6 Collector/Code/TrialTypes/FREQ/display.php
@@ -0,0 +1,26 @@
+<?php
+    if (!isset($text) || $text === '') { 
+        $text = 'How often are these two words used together?|Type your response on a scale from 0-100 and then press Enter.';
+    }
+
+    $texts = explode('|', $text);
+    $mainText = array_shift($texts);
+?>
+
+<div class="textcenter">
+    <div><?php echo isset($text) ? $text : ""; ?></div>
+
+<br>
+
+<div class="study">
+    <span class="study-left"   ><?php echo $cue;    ?></span>
+    <span class="study-divider"><?php echo ":";     ?></span>
+    <span class="study-right"  ><?php echo $answer; ?></span>
+    <br>
+
+<br>
+  
+<div class="textcenter">
+    <input name="JOL" type="text" value="" autocomplete="off" class="forceNumeric textcenter collectorInput">
+    <button class="collectorButton" id="FormSubmitButton">Submit</button>
+</div>
